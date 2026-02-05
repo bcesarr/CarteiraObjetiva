02095a0ba09345c3a4ccfdd6104da3d8b3d190b0
Bruno Cesar <brunocesar.as7@gmail.com>
2026-02-05 06:29:24 +0000

    Add: criando estrutura de controle, saldo e modelo e primeiras apis get

--- src/main/java/com/carteiraobjetiva/carteiraobjetiva/controller/HelloController.java
+++ src/main/java/com/carteiraobjetiva/carteiraobjetiva/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.carteiraobjetiva.carteiraobjetiva;
+package com.carteiraobjetiva.carteiraobjetiva.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/com/carteiraobjetiva/carteiraobjetiva/controller/SaldoController.java
@@ -0,0 +1,19 @@
+package com.carteiraobjetiva.carteiraobjetiva.controller;
+
+import com.carteiraobjetiva.carteiraobjetiva.service.SaldoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SaldoController {
+    private final SaldoService saldoService;
+
+    public SaldoController(SaldoService saldoService) {
+        this.saldoService = saldoService;
+    }
+
+    @GetMapping("/saldo")
+    public double obterSaldo() {
+        return saldoService.obterSaldo();
+    }
+}
