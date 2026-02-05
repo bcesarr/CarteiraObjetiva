49740d6a7ac37fbe7f5b047fe0694734e9def3c0
Bruno Cesar <brunocesar.as7@gmail.com>
2026-02-05 05:35:22 +0000

    Primeiro commit - ambiente ajustado

---
+++ src/main/java/com/carteiraobjetiva/carteiraobjetiva/CarteiraObjetivaApplication.java
@@ -0,0 +1,13 @@
+package com.carteiraobjetiva.carteiraobjetiva.carteiraobjetiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraObjetivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteiraObjetivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carteiraobjetiva/carteiraobjetiva/CarteiraObjetivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carteiraobjetiva.carteiraobjetiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraObjetivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
