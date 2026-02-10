3c42647aefd6bc22c57ca5dc34ea73a42d0e32df
Bruno Cesar <brunocesar.as7@gmail.com>
2026-02-10 20:21:07 +0000

    Feat: Add Lombok no projeto (nos DTOS)

--- src/main/java/com/carteiraobjetiva/carteiraobjetiva/dto/CriarContaRequestDTO.java
+++ src/main/java/com/carteiraobjetiva/carteiraobjetiva/dto/CriarContaRequestDTO.java
@@ -1,23 +1,36 @@
 package com.carteiraobjetiva.carteiraobjetiva.dto;
 
+// (Agora) Uso do Lombok para gerar getters e setters automaticamente
+import lombok.Data;
+
+@Data
 public class CriarContaRequestDTO {
     
     private String nomeTitular;
     private double saldoInicial;
-
-    public String getNomeTitular() {
-        return nomeTitular;
-    }
-
-    public void setNomeTitular(String nomeTitular) {
-        this.nomeTitular = nomeTitular;
-    }
-
-    public double getSaldoInicial() {
-        return saldoInicial;
-    }
-
-    public void setSaldoInicial(double saldoInicial) {
-        this.saldoInicial = saldoInicial;
-    }
 }
+
+// -------------------------------- \\
+
+// (Antes) Sem o uso do Lombok
+// public class CriarContaRequestDTO {
+    
+//     private String nomeTitular;
+//     private double saldoInicial;
+
+//     public String getNomeTitular() {
+//         return nomeTitular;
+//     }
+
+//     public void setNomeTitular(String nomeTitular) {
+//         this.nomeTitular = nomeTitular;
+//     }
+
+//     public double getSaldoInicial() {
+//         return saldoInicial;
+//     }
+
+//     public void setSaldoInicial(double saldoInicial) {
+//         this.saldoInicial = saldoInicial;
+//     }
+// }
--- src/main/java/com/carteiraobjetiva/carteiraobjetiva/dto/TransferenciaRequestDTO.java
+++ src/main/java/com/carteiraobjetiva/carteiraobjetiva/dto/TransferenciaRequestDTO.java
@@ -1,31 +1,44 @@
 package com.carteiraobjetiva.carteiraobjetiva.dto;
 
+// (Agora) Uso do Lombok para gerar getters e setters automaticamente
+import lombok.Data;
+
+@Data
 public class TransferenciaRequestDTO {
     private Long idOrigem;
     private Long idDestino;
     private double valor;
+}
 
-    public Long getIdOrigem() {
-        return idOrigem;
-    }
+// -------------------------------- \\
 
-    public void setIdOrigem(Long idOrigem) {
-        this.idOrigem = idOrigem;
-    }
+// (Antes) Sem o uso do Lombok
+// public class TransferenciaRequestDTO {
+//     private Long idOrigem;
+//     private Long idDestino;
+//     private double valor;
 
-    public Long getIdDestino() {
-        return idDestino;
-    }
+//     public Long getIdOrigem() {
+//         return idOrigem;
+//     }
 
-    public void setIdDestino(Long idDestino) {
-        this.idDestino = idDestino;
-    }
+//     public void setIdOrigem(Long idOrigem) {
+//         this.idOrigem = idOrigem;
+//     }
 
-    public double getValor() {
-        return valor;
-    }
+//     public Long getIdDestino() {
+//         return idDestino;
+//     }
 
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-}   
+//     public void setIdDestino(Long idDestino) {
+//         this.idDestino = idDestino;
+//     }
+
+//     public double getValor() {
+//         return valor;
+//     }
+
+//     public void setValor(double valor) {
+//         this.valor = valor;
+//     }
+// }   
